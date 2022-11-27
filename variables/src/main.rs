cd13bb690c6cd848a06dd25cf5eba40fb4690476
Marco Antônio <ma.souza.junior@gmail.com>
2022-11-27 11:42:04 -0300

    fea: add shadowing example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,21 @@
 fn main() {
+    mut_var();
+    shadowing();
+}
+
+fn shadowing() {
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("This value x in the inner scope is: {x}");
+    }
+
+    println!("This value x is: {x}");
+}
+
+fn mut_var() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
